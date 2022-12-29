85e30d27cde3ea9d34dd15f6148318f25b9a7f3e
abhiroopdutta <abhiroopd@ymail.com>
2022-12-29 19:39:38 +0000

    Basic mobile home page implemented

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<div class="layout">
+	<div class="nav-container">
+		<nav>
+			<a href="/">Home</a>
+			<a href="/">Projects</a>
+			<a href="/">About</a>
+		</nav>
+	</div>
+	<slot />
+</div>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@600;800&family=Bungee+Shade&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<style>
+	.layout {
+		margin: min(20vh, 2rem) min(5vw, 2rem);
+	}
+
+	.nav-container {
+		display: flex;
+	}
+
+	@media (min-width: 40em) {
+		.nav-container {
+			justify-content: end;
+		}
+	}
+
+	nav {
+		display: flex;
+		justify-content: space-between;
+		background-color: bisque;
+		padding: 0.8rem 1rem;
+		border: 2px solid #3d3c3c;
+		border-radius: 26px;
+		max-width: min(100vw, 20rem);
+	}
+
+	a {
+		text-decoration: none;
+		text-transform: uppercase;
+		font-family: 'Big Shoulders Display', cursive;
+		font-weight: 800;
+		color: #3d3c3c;
+		padding: 0 min(10vw, 1.5rem);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,74 @@
+<div class="parent-container">
+	<header>
+		<h1 class="profession-first">Web</h1>
+		<h1 class="profession-last">Developer</h1>
+	</header>
+
+	<section>
+		<h1 class="name">Abhiroop</h1>
+		<ul>
+			<li>react</li>
+			<li>flask</li>
+			<li>mongo</li>
+			<li>mysql</li>
+			<li>docker</li>
+			<li>nginx</li>
+			<li>git</li>
+			<li>bash</li>
+			<li>aws ec2</li>
+		</ul>
+	</section>
+</div>
+
+<style>
+	:root {
+		background-color: #ff8e8e;
+	}
+
+	.profession-first,
+	.profession-last {
+		font-family: 'Bungee Shade', cursive;
+		text-transform: uppercase;
+		color: #282727;
+		line-height: 1rem;
+		font-weight: 400;
+	}
+
+	.profession-first {
+		font-size: 6rem;
+	}
+	.profession-last {
+		font-size: 3rem;
+	}
+
+	.name {
+		font-family: 'Big Shoulders Display', cursive;
+		text-transform: uppercase;
+		color: #282727;
+		writing-mode: vertical-lr;
+		transform: rotate(180deg);
+		font-size: 9rem;
+		line-height: 0;
+		font-weight: 800;
+		margin: 0 3.8rem;
+	}
+
+	section {
+		display: flex;
+		align-items: flex-end;
+	}
+
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0 1rem;
+	}
+	li {
+		font-family: 'Big Shoulders Display', cursive;
+		font-weight: 600;
+		text-transform: uppercase;
+		color: #282727;
+		font-size: 1.8rem;
+		letter-spacing: 1rem;
+	}
+</style>
